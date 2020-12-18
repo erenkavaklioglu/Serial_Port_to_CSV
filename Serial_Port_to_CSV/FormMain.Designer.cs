db46b07ee2d583b8a0ebe8fa34cd44966d46f9de
Eren Kavaklıoğlu <erenkavaklioglu@yahoo.com>
2020-12-18 17:12:53 +0300

    Plotter is added to application. Not healthy for now.

--- Serial_Port_to_CSV/FormMain.Designer.cs
+++ Serial_Port_to_CSV/FormMain.Designer.cs
@@ -28,20 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.splitContainerToolbarMain = new System.Windows.Forms.SplitContainer();
-            this.listBoxMessages = new System.Windows.Forms.ListBox();
             this.toolStripToolbar = new System.Windows.Forms.ToolStrip();
-            this.saveFileDialogCSVFile = new System.Windows.Forms.SaveFileDialog();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripButtonStart = new System.Windows.Forms.ToolStripButton();
             this.toolStripButtonStop = new System.Windows.Forms.ToolStripButton();
             this.toolStripButtonConfigurations = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripButtonClear = new System.Windows.Forms.ToolStripButton();
+            this.listBoxMessages = new System.Windows.Forms.ListBox();
+            this.saveFileDialogCSVFile = new System.Windows.Forms.SaveFileDialog();
+            this.splitContainerListPlotter = new System.Windows.Forms.SplitContainer();
+            this.chartPlotter = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.toolStripComboBoxWriteToFile = new System.Windows.Forms.ToolStripComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerToolbarMain)).BeginInit();
             this.splitContainerToolbarMain.Panel1.SuspendLayout();
             this.splitContainerToolbarMain.Panel2.SuspendLayout();
             this.splitContainerToolbarMain.SuspendLayout();
             this.toolStripToolbar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerListPlotter)).BeginInit();
+            this.splitContainerListPlotter.Panel1.SuspendLayout();
+            this.splitContainerListPlotter.Panel2.SuspendLayout();
+            this.splitContainerListPlotter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPlotter)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainerToolbarMain
@@ -60,22 +71,11 @@
             // 
             // splitContainerToolbarMain.Panel2
             // 
-            this.splitContainerToolbarMain.Panel2.Controls.Add(this.listBoxMessages);
+            this.splitContainerToolbarMain.Panel2.Controls.Add(this.splitContainerListPlotter);
             this.splitContainerToolbarMain.Size = new System.Drawing.Size(846, 387);
             this.splitContainerToolbarMain.SplitterDistance = 40;
             this.splitContainerToolbarMain.SplitterWidth = 2;
             this.splitContainerToolbarMain.TabIndex = 0;
-            // 
-            // listBoxMessages
-            // 
-            this.listBoxMessages.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBoxMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.listBoxMessages.FormattingEnabled = true;
-            this.listBoxMessages.ItemHeight = 16;
-            this.listBoxMessages.Location = new System.Drawing.Point(0, 0);
-            this.listBoxMessages.Name = "listBoxMessages";
-            this.listBoxMessages.Size = new System.Drawing.Size(844, 343);
-            this.listBoxMessages.TabIndex = 0;
             // 
             // toolStripToolbar
             // 
@@ -87,23 +87,12 @@
             this.toolStripButtonStop,
             this.toolStripButtonConfigurations,
             this.toolStripSeparator1,
-            this.toolStripButtonClear});
+            this.toolStripButtonClear,
+            this.toolStripComboBoxWriteToFile});
             this.toolStripToolbar.Location = new System.Drawing.Point(0, 0);
             this.toolStripToolbar.Name = "toolStripToolbar";
             this.toolStripToolbar.Size = new System.Drawing.Size(844, 38);
             this.toolStripToolbar.TabIndex = 0;
-            // 
-            // saveFileDialogCSVFile
-            // 
-            this.saveFileDialogCSVFile.DefaultExt = "csv";
-            this.saveFileDialogCSVFile.Filter = "Comma Separated Value Files|*.csv";
-            this.saveFileDialogCSVFile.InitialDirectory = "C:\\";
-            this.saveFileDialogCSVFile.RestoreDirectory = true;
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
             // 
             // toolStripButtonStart
             // 
@@ -134,6 +123,11 @@
             this.toolStripButtonConfigurations.Text = "Configurations";
             this.toolStripButtonConfigurations.Click += new System.EventHandler(this.toolStripButtonConfigurations_Click);
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
+            // 
             // toolStripButtonClear
             // 
             this.toolStripButtonClear.Image = global::Serial_Port_to_CSV.Properties.Resources.Oxygen_Clear_Icon_128_128_1;
@@ -142,6 +136,72 @@
             this.toolStripButtonClear.Size = new System.Drawing.Size(68, 35);
             this.toolStripButtonClear.Text = "Clear";
             this.toolStripButtonClear.Click += new System.EventHandler(this.toolStripButtonClear_Click);
+            // 
+            // listBoxMessages
+            // 
+            this.listBoxMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxMessages.FormattingEnabled = true;
+            this.listBoxMessages.ItemHeight = 16;
+            this.listBoxMessages.Location = new System.Drawing.Point(0, 0);
+            this.listBoxMessages.Name = "listBoxMessages";
+            this.listBoxMessages.Size = new System.Drawing.Size(298, 343);
+            this.listBoxMessages.TabIndex = 0;
+            // 
+            // saveFileDialogCSVFile
+            // 
+            this.saveFileDialogCSVFile.DefaultExt = "csv";
+            this.saveFileDialogCSVFile.Filter = "Comma Separated Value Files|*.csv";
+            this.saveFileDialogCSVFile.InitialDirectory = "C:\\";
+            this.saveFileDialogCSVFile.RestoreDirectory = true;
+            // 
+            // splitContainerListPlotter
+            // 
+            this.splitContainerListPlotter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainerListPlotter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerListPlotter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainerListPlotter.IsSplitterFixed = true;
+            this.splitContainerListPlotter.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerListPlotter.Name = "splitContainerListPlotter";
+            // 
+            // splitContainerListPlotter.Panel1
+            // 
+            this.splitContainerListPlotter.Panel1.Controls.Add(this.listBoxMessages);
+            // 
+            // splitContainerListPlotter.Panel2
+            // 
+            this.splitContainerListPlotter.Panel2.Controls.Add(this.chartPlotter);
+            this.splitContainerListPlotter.Size = new System.Drawing.Size(846, 345);
+            this.splitContainerListPlotter.SplitterDistance = 300;
+            this.splitContainerListPlotter.SplitterWidth = 2;
+            this.splitContainerListPlotter.TabIndex = 1;
+            // 
+            // chartPlotter
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartPlotter.ChartAreas.Add(chartArea1);
+            this.chartPlotter.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chartPlotter.Legends.Add(legend1);
+            this.chartPlotter.Location = new System.Drawing.Point(0, 0);
+            this.chartPlotter.Name = "chartPlotter";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Blue;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartPlotter.Series.Add(series1);
+            this.chartPlotter.Size = new System.Drawing.Size(542, 343);
+            this.chartPlotter.TabIndex = 0;
+            this.chartPlotter.Text = "chart1";
+            // 
+            // toolStripComboBoxWriteToFile
+            // 
+            this.toolStripComboBoxWriteToFile.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.toolStripComboBoxWriteToFile.Items.AddRange(new object[] {
+            "Write To File",
+            "Only Screen"});
+            this.toolStripComboBoxWriteToFile.Name = "toolStripComboBoxWriteToFile";
+            this.toolStripComboBoxWriteToFile.Size = new System.Drawing.Size(121, 38);
             // 
             // FormMain
             // 
@@ -160,6 +220,11 @@
             this.splitContainerToolbarMain.ResumeLayout(false);
             this.toolStripToolbar.ResumeLayout(false);
             this.toolStripToolbar.PerformLayout();
+            this.splitContainerListPlotter.Panel1.ResumeLayout(false);
+            this.splitContainerListPlotter.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerListPlotter)).EndInit();
+            this.splitContainerListPlotter.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartPlotter)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -175,6 +240,9 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialogCSVFile;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton toolStripButtonClear;
+        private System.Windows.Forms.SplitContainer splitContainerListPlotter;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPlotter;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxWriteToFile;
     }
 }
 
--- Serial_Port_to_CSV/FormMain.cs
+++ Serial_Port_to_CSV/FormMain.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Configuration;
+using System.Data;
+using System.Drawing;
 using System.IO;
 using System.IO.Ports;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Serial_Port_to_CSV
 {
@@ -14,6 +17,22 @@ namespace Serial_Port_to_CSV
 
         string _csvFilePath;
 
+        DataTable _plotterData;
+
+        int _plotCounter;
+
+        #endregion
+
+        #region Properties
+
+        private bool IsFileWriting
+        {
+            get
+            {
+                return (0 == toolStripComboBoxWriteToFile.SelectedIndex);
+            }
+        }
+
         #endregion
 
         #region Constructors
@@ -22,6 +41,9 @@ namespace Serial_Port_to_CSV
         {
             InitializeComponent();
             InitializeSerialPort();
+            InitializePlotter();
+
+            toolStripComboBoxWriteToFile.SelectedIndex = 0;
         }
 
         #endregion
@@ -30,8 +52,19 @@ namespace Serial_Port_to_CSV
 
         private void toolStripButtonStart_Click(object sender, EventArgs e)
         {
-            if (SetCSVFilePath())
+            bool statusOK = true;
+
+            if (IsFileWriting)
             {
+                statusOK = false;
+                statusOK = SetCSVFilePath();
+            }
+
+            if (statusOK)
+            {
+                _plotCounter = 0;
+                chartPlotter.Series[0].Points.Clear();
+
                 ChangeControlStates(false, true, false);
                 _serialReader.Open();
             }
@@ -46,6 +79,10 @@ namespace Serial_Port_to_CSV
         private void toolStripButtonClear_Click(object sender, EventArgs e)
         {
             listBoxMessages.Items.Clear();
+
+            _plotCounter = 0;
+            chartPlotter.Series[0].Points.Clear();
+            _plotterData.Rows.Clear();
         }
 
         private void toolStripButtonConfigurations_Click(object sender, EventArgs e)
@@ -57,6 +94,26 @@ namespace Serial_Port_to_CSV
         #endregion
 
         #region Methods - Private
+
+        private void InitializePlotter()
+        {
+            _plotCounter = 0;
+            _plotterData = new DataTable();
+            _plotterData.Columns.Add("Point_X", typeof(double));
+            _plotterData.Columns.Add("Signal_Y", typeof(double));
+
+            chartPlotter.DataSource = _plotterData;
+            chartPlotter.Series["Series1"].XValueMember = "Point_X";
+            chartPlotter.Series["Series1"].YValueMembers = "Signal_Y";
+            chartPlotter.Series["Series1"].ChartType = SeriesChartType.Line;
+            chartPlotter.Series["Series1"].BorderWidth = 2;
+
+            chartPlotter.ChartAreas[0].AxisY.LabelStyle.Format = "";
+            chartPlotter.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.LightGray;
+            chartPlotter.ChartAreas[0].AxisX.MajorGrid.LineWidth = 1;
+            chartPlotter.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.LightGray;
+            chartPlotter.ChartAreas[0].AxisY.MajorGrid.LineWidth = 1;
+        }
 
         private void InitializeSerialPort()
         {
@@ -70,8 +127,13 @@ namespace Serial_Port_to_CSV
 
             _serialReader = new SerialPortReader(port, baudRate, parity, dataBits, stopBits);
 
-            _serialReader.AddOperation(new SerialPortReader.PerformOperation(WriteValueToFile));
+            if (IsFileWriting)
+            {
+                _serialReader.AddOperation(new SerialPortReader.PerformOperation(WriteValueToFile));
+            }
+
             _serialReader.AddOperation(new SerialPortReader.PerformOperation(WriteValueToListBox));
+            _serialReader.AddOperation(new SerialPortReader.PerformOperation(WriteValueToPlotter));
         }
 
         private bool SetCSVFilePath()
@@ -96,10 +158,14 @@ namespace Serial_Port_to_CSV
 
         private void WriteValueToListBox(string value)
         {
-            this.Invoke(new Action(() =>
+            try
             {
-                listBoxMessages.Items.Insert(0, value);
-            }));
+                this.Invoke(new Action(() =>
+                {
+                    listBoxMessages.Items.Insert(0, value);
+                }));
+            }
+            catch(Exception) { }
         }
 
         private void WriteValueToFile(string value)
@@ -110,6 +176,29 @@ namespace Serial_Port_to_CSV
                 {
                     sWriter.Write(value);
                 }
+            }));
+        }
+
+        private void WriteValueToPlotter(string value)
+        {
+            this.Invoke(new Action(() =>
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        if (value.Contains("\r"))
+                        {
+                            value = value.Replace("\r", "");
+                        }
+
+                        _plotterData.Rows.Add(_plotCounter, Convert.ToDouble(value));
+                        chartPlotter.DataBind();
+
+                        _plotCounter++;
+                    }
+                }
+                catch(Exception) { }
             }));
         }
 
